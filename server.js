72a64733f4f3b12782f0bc3adb6315f162fb86b3
Ben <shippeyben@gmail.com>
2017-04-02 17:21:38 +0000

    Added NodeMailer logic for first time

--- server.js
+++ server.js
@@ -13,6 +13,36 @@ app.post('/submit', upload.array(), function (req, res, next) {
     console.log(req.body.phone);
     console.log(req.body.email);
     res.end('Thanks for your submission');
+    
+    //begin nodemailer logic
+    'use strict';
+    const nodemailer = require('nodemailer');
+    
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'gmail.user@gmail.com',
+            pass: 'yourpass'
+        }
+    });
+    
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: req.body.email,// sender address
+        to: 'shippeyben@gmail.com', // list of receivers
+        subject: 'Submission from Portfolio Site', // Subject line
+        text: req.body.message, // plain text body
+        html: '<b>' + req.body.message + '</b></b>' + 'Phone:' + req.body.phone // html body
+    };
+    
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+    });
 })
 
 app.listen(8080);
