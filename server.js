87214dadbc6276e4f41cf6d2c69356a8279b2ec4
Ben <shippeyben@gmail.com>
2017-04-02 17:13:14 +0000

    added multer middleware, .post() express method and callback logic

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var path = require('path');
+var multer = require('multer');
+var upload = multer();
 var app = express();
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
+})
+ 
+app.post('/submit', upload.array(), function (req, res, next) {
+    console.log(req.body.message);
+    console.log(req.body.phone);
+    console.log(req.body.email);
+    res.end('Thanks for your submission');
 })
 
 app.listen(8080);
